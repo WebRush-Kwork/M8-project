48cc0e215faddfefbf01a4d95b4300e5298ce852
gleb <webrushgithub@gmail.com>
2024-06-13 17:23:34 +0500

    good progress, many advantages

--- main.py
+++ main.py
@@ -26,92 +26,106 @@ def deep_pavlov_answer(question):
 @bot.message_handler(commands=['help'])
 def help(message):
     bot.reply_to(message,
-                 'Вы попали в отдел помощи навигации по боту! <b>Доступные команды:</b>\n/start - приветствие бота и инструкция',
-                 parse_mode='HTML')
+                 'Вы попали в отдел помощи навигации по боту! Доступные команды:\n/start - приветствие бота и инструкция 📝\n/info - информация о пользователе, если она была занесена ℹ️\n/help - возможности бота, навигация 🧭')
 
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
+def start(message):
     bot.reply_to(message,
-                 'Здравствуйте! Для начала мне потребуется узнать некоторую информацию о Вас для того, чтобы решить, как Вы можете изменить свою карьеру! 📝\nМне потребуется: текущая должность, предпочтения, ощущения от работы и некоторые детали по желанию.')
+                 'Здравствуйте! Для начала мне потребуется узнать некоторую информацию о Вас для того, чтобы решить, как Вы можете изменить свою карьеру! 🌎📝\n\nМне потребуется: текущая профессия, предпочтения, ощущения от работы и некоторые детали по желанию.')
 
     keyboard = InlineKeyboardMarkup()
     b1 = InlineKeyboardButton(text="Да", callback_data='yes')
     b2 = InlineKeyboardButton(text="Нет", callback_data='no')
     keyboard.add(b1)
     keyboard.add(b2)
+    time.sleep(1)
     bot.send_message(
         message.chat.id, 'Готовы ли Вы сейчас ответить на некоторые вопросы?', reply_markup=keyboard)
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def handle_callback_query(call):
-    if call.data == "yes":
-        bot.send_message(call.message.chat.id, 'Хорошо, давайте начнем 🔥\nКакая у Вас сейчас должность 💼?')
-        bot.register_next_step_handler(call.message, job_handler)
-    elif call.data == "no":
-        bot.send_message(call.message.chat.id, "Без проблем, я буду ждать Вас.")
-    bot.delete_message(call.message.chat.id, call.message.message_id)
-    bot.delete_message(call.message.chat.id, call.message.message_id - 1)
-
-
 def job_handler(message):
-    bot.reply_to(message, 'Интересная профессия! Перейдем ко второму вопросу.')
-    person_info[message.chat.id] = {"job": message.text}
+    bot.delete_message(message.chat.id, message.message_id)
+    bot.delete_message(message.chat.id, message.message_id - 1)
     bot.send_message(message.chat.id,
-                     'Какие у Вас интересы? (рисование, программирование, черчение)\nМожете написать несколько хобби.', )
+                     'Интересная профессия! Перейдем ко второму вопросу. \nКакие у Вас интересы 🎨🎤? Можете написать несколько хобби 🔢')
+    person_info[message.chat.id] = {"job": message.text}
     bot.register_next_step_handler(message, interests_handler)
 
 
 def interests_handler(message):
-    bot.reply_to(message, 'Отлично! Перейдем к третьему вопросу.')
+    bot.delete_message(message.chat.id, message.message_id)
+    bot.delete_message(message.chat.id, message.message_id - 1)
+
     person_info[message.chat.id]["interests"] = message.text
-    markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    itembtn1 = KeyboardButton('Хорошие')
-    itembtn2 = KeyboardButton('Плохие')
-    itembtn3 = KeyboardButton('По-разному')
-    markup.add(itembtn1, itembtn2, itembtn3)
+    markup = InlineKeyboardMarkup(row_width=3)
+    itembtn1 = InlineKeyboardButton('👍', callback_data='good')
+    itembtn2 = InlineKeyboardButton('👎', callback_data='bad')
+    itembtn3 = InlineKeyboardButton('🤷‍', callback_data='mixed')
+    markup.add(itembtn1, itembtn2, itembtn3)  # problem with displaying the prompt, it shows this question
     bot.send_message(message.chat.id,
-                     'Какие у Вас ощущение от работы и все, что с ней связано? (хорошие, плохие, по-разному).',
+                     'Отлично! Перейдем к третьему вопросу. \nКакие у Вас ощущение от работы и все, что с ней связано 🧐?',
                      reply_markup=markup)
-    bot.register_next_step_handler(message, feelings_handler)
+
+
+@bot.callback_query_handler(func=lambda call: call.data in ["yes", "no"])
+def handle_yes_no_callback(call):
+    if call.data == "yes":
+        bot.reply_to(call.message, 'Хорошо, давайте начнем 🔥\nКакая у Вас сейчас профессия 💼?')
+        bot.delete_message(call.message.chat.id, call.message.message_id)
+        bot.delete_message(call.message.chat.id, call.message.message_id - 1)
+        bot.register_next_step_handler(call.message, job_handler)
+    elif call.data == "no":
+        bot.send_message(call.message.chat.id, "Без проблем, я буду ждать Вас 🕒")
+        bot.delete_message(call.message.chat.id, call.message.message_id)
+        bot.delete_message(call.message.chat.id, call.message.message_id - 1)
+
+
+@bot.callback_query_handler(func=lambda call: call.data in ['good', 'bad', 'mixed'])
+def handle_feelings_callback(call):
+    if call.data == 'good':
+        person_info[call.message.chat.id]["feelings"] = 'Хорошие'
+        bot.delete_message(call.message.chat.id, call.message.message_id)
+        feelings_handler(call.message)
+    elif call.data == 'bad':
+        person_info[call.message.chat.id]["feelings"] = 'Плохие'
+        bot.delete_message(call.message.chat.id, call.message.message_id)
+        feelings_handler(call.message)
+    elif call.data == 'mixed':
+        person_info[call.message.chat.id]["feelings"] = 'По-разному'
+        bot.delete_message(call.message.chat.id, call.message.message_id)
+        feelings_handler(call.message)
 
 
 def feelings_handler(message):
-    bot.reply_to(message,
-                 'Отлично! Перейдем к последнему вопросу. Какие у Вас мысли, которые возникают в процессе работы 🧘?',
-                 reply_markup=ReplyKeyboardRemove())
+    bot.send_message(message.chat.id,
+                     'Отлично! Перейдем к последнему вопросу. \nКакие мысли Вас сопровождают в процессе работы 🧘?',
+                     reply_markup=ReplyKeyboardRemove())
     person_info[message.chat.id]["feelings"] = message.text
     bot.register_next_step_handler(message, general_info_handler)
 
 
 def general_info_handler(message):
+    bot.delete_message(message.chat.id, message.message_id)
+    bot.delete_message(message.chat.id, message.message_id - 1)
     global prompt
     person_info[message.chat.id]["general"] = message.text
     person_data = person_info.pop(message.chat.id)
     person.info(message.from_user.id, person_data["job"], person_data["interests"], person_data["feelings"],
                 person_data["general"])
     prompt = f'Моя профессия: {person_data["job"]}. \nМои интересы: {person_data["interests"]}. \nМои ощущение от работы: {person_data["feelings"]}. \nМои мысли во времы работы: {person_data["general"]}. \nЯ хочу сменить род деятельности. Какие профессии или хобби можешь посоветовать?'
-    sent_message = bot.reply_to(message,
-                                'Спасибо! Ваша информация добавлена в базу данных.\nПриступил к приготовлению информации для Вас')
+    sent_message = bot.send_message(message.chat.id,
+                                    'Спасибо! Ваша информация добавлена в базу данных.\nПриступил к приготовлению ответа для Вас')
 
-    loading_text = 'Спасибо! Ваша информация добавлена в базу данных.\nПриступил к приготовлению информации для Вас'
+    loading_text = 'Спасибо! Ваша информация добавлена в базу данных.\nПриступил к приготовлению ответа для Вас'
     for _ in range(5):
         for dots in range(3, 0, -1):
             bot.edit_message_text(chat_id=sent_message.chat.id, message_id=sent_message.message_id,
                                   text=loading_text + "." * dots)
             time.sleep(0.2)
+    result = deep_pavlov_answer(prompt)
     bot.edit_message_text(chat_id=sent_message.chat.id, message_id=sent_message.message_id,
-                          text='Финальное сообщение')
-
-    bot.register_next_step_handler(message, ai_answer)
-
-
-def ai_answer(message):
-    global prompt
-    result = deep_pavlov_answer('who is the first president of the US?')
-    bot.send_message(message.chat.id, result)
-    bot.send_message(message.chat.id, prompt)
+                          text=f'{result}\n\n{prompt}')
 
 
 if __name__ == '__main__':
